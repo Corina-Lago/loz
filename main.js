039542afeb3d8b7a049c56ba610ac3e6aaf35745
Corina-Lago <corinalago7@gmail.com>
2025-04-30 16:46:57 -0300

    loz

---
+++ main.js
@@ -0,0 +1,11 @@
+
+const apagarBtn = document.getElementById('boton')
+
+apagarBtn.addEventListener('click', () => {
+console.log('El boton apagar fue presionado');
+document.body.classList.toggle('apagado');
+document.body.classList.toggle('encendido')
+
+apagarBtn.textContent = document.body.classList.contains('encendido')? 'APAGAR LA LUZ' : 'PRENDER LUZ';
+
+})
